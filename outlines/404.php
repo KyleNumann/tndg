70c1383e89e568fbfaaca21af92c2303e974d7c1
KyleNumann <kyle.numann@gmail.com>
2015-08-10 12:55:11 -0500

    added outlines theme

---
+++ outlines/404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying 404 pages.
+ */
+
+get_header(); ?>
+
+		<div id="primary" class="content-area">
+			<div id="content" class="site-content" role="main">
+
+				<h1 class="entry-title">Page not found</h1>
+				<p>Try searching for what you were looking for:</p>
+				<?php get_search_form(); ?>
+
+			</div><!-- #content .site-content -->
+		</div><!-- #primary .content-area -->
+
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
